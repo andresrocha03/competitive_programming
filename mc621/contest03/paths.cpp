928e9cc597256876a647816dc041b1bc86fb501d
andresantosr <andresanro03@gmail.com>
2024-08-29 22:27:56 -0300

    time limit

--- mc621/contest03/paths.cpp
+++ mc621/contest03/paths.cpp
@@ -1,60 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define MAX 101 
 typedef long long ll ;
 
 ll MOD = 1000000007;
 
-struct Matrix {
-    int m;
-    std::vector<std::vector<ll>> mat;
+ll in[MAX][MAX];
+ll ans[MAX][MAX];
+ll prod[MAX][MAX];
 
-    // Constructor
-    Matrix(int size) : m(size), mat(size, std::vector<ll>(size)) {}
-
-};
-
-
-Matrix matMul(Matrix& a, Matrix& b, int size) {
-    Matrix ans(size);
+//o codigo abaixo foi retirado dos slides da aula e adaptado ao problema
+void matMuli(ll a[MAX][MAX], ll b[MAX][MAX], ll size) {
     ll i, j, k;
-    
     // Perform matrix multiplication
     for (i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
+            prod[i][j] = 0;
             for (k = 0; k < size; k++) {
-                ans.mat[i][j] += a.mat[i][k] * b.mat[k][j];
-                ans.mat[i][j] %= MOD;
+                prod[i][j] += a[i][k] * b[k][j];
+                prod[i][j] = prod[i][j]%MOD;
             }
         }
     }
-    
-    return ans;
 }
 
-Matrix matPow(Matrix base, unsigned long long n, int size) {
-    Matrix ans(size);
+void matPow(unsigned long long n, ll size) {
     ll i, j;
     
     // Initialize the answer as the identity matrix
     for (i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
-            ans.mat[i][j] = (i == j);
+            ans[i][j] = (i == j);
         }
     }
     
     // Perform matrix exponentiation
     while (n > 0) {
         if (n % 2 == 1) {
-            ans = matMul(ans, base, size);
+            // int res[MAX][MAX];
+            matMuli(ans, in, size);
+            for (i = 0; i < size; i++) {
+                for (j = 0; j < size; j++) {
+                    ans[i][j] = prod[i][j];
+                }
+            }
         }
-        base = matMul(base, base, size);
+        matMuli(in, in, size);
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+                in[i][j] = prod[i][j];
+            }
+        }
         n /= 2;
     }
     
-    return ans;
 }
-
+//o codigo acima foi retirado dos slides da aula e adaptado ao problema
 
 int main() {
     ios::sync_with_stdio(0);
@@ -64,13 +65,12 @@ int main() {
 
     cin >> n >> m >> k;
     
-    Matrix res(n);
     for (int i=0; i<m; i++) {
         cin >> a >> b;
-        res.mat[a-1][b-1]+=1;
+        in[a-1][b-1]+=1;
     }
 
-    res = matPow(res,k, n);
-    int num = res.mat[0][n-1];
+    matPow(k, n);
+    ll num = ans[0][n-1];
     cout << num;
 }
