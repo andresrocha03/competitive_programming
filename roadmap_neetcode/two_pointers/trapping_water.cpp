b124cc302e242244f460231cf451c0e390ad6a9e
andresrocha03 <andresanro03@gmail.com>
2026-07-16 16:54:07 -0300

    trapping water

--- roadmap_neetcode/two_pointers/trapping_water.cpp
+++ roadmap_neetcode/two_pointers/trapping_water.cpp
@@ -1,29 +1,25 @@
 //Problem: Given an elevation map, find maximum area of water that can be trapped between bars.
-//Sol: Use two pointers, fix left pointer in i, move right pointer from i+1 to the right accounting possible water trapping, move left pointer as soon as a bar greater or equal to i is found
+//Sol: Use two pointers, left pointer at the begin and right and right at the end, they will limit the maximum amount of water that can be trapped in a certain position
 
 class Solution {
 public:
     int trap(vector<int>& height) {
 
-        int l=0, r=1;
-        int res=0;
-        int cur=0;
-        stack<int> lefts;
-        lefts.push(l);
-        while (r < height.size()){
+        int l = 0; 
+        int r = height.size()-1;
+        int lmax = 0;
+        int rmax = 0;
+        int res = 0;
+        while (l<r){
             if (height[l]<=height[r]) {
-                res += cur;
-                cur =0;
-                lefts.pop();
-                if (!lefts.empty()) {
-                    l = lefts.top();
-                }
-                else  l=r;
+                lmax = max(lmax, height[l]);
+                res += lmax - height[l];
+                l++;
             }
             else {
-                cur += (height[l]-height[r]);
-                l = r++;
-                lefts.push(l);
+                rmax = max(rmax, height[r]);
+                res += rmax - height[r];
+                r--;
             }
         }
         return res;
